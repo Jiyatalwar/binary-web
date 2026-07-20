435dcac9f2d404d1157e0215297997f3cabb6cf4
Jiyatalwar <jaya.dewan@binaryglobal.com>
2026-07-20 14:07:10 +0530

    update metadeta

--- .history/app/layout_20260716132024.tsx
+++ .history/app/layout_20260716132024.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
-  title: "Binary Global web clone",
+  title: "Binary Global Solutions",
   description: "State-of-the-art technological infrastructures to secure, automate, and accelerate operations.",
 };
 
