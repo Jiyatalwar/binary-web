2573f860479e047a559b0df74143263d69cd2caa
# 👋 Hi, I'm Jiya talwar <166608289+Jiyatalwar@users.noreply.github.com>
2026-07-20 14:05:10 +0530

    Update metadata title for web-clone

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
-  title: "Binary Global Solutions",
+  title: "Binary Global web-clone ",
   description: "State-of-the-art technological infrastructures to secure, automate, and accelerate operations.",
 };
 
